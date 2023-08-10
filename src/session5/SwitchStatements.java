2770a5c171af72edea9816ccbd6bc72d9cb48c01
Ana Popa <bolboceanu_ana@yahoo.com>
2023-08-10 19:09:24 +0300

    Session 5

---
+++ src/session5/SwitchStatements.java
@@ -0,0 +1,10 @@
+package session5;
+
+import java.util.Scanner;
+
+public class SwitchStatements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int day = scanner.nextInt()
+    }
+}
